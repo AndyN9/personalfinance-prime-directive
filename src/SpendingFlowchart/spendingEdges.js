e451f5aa1b19def005b131bf7a97da91136a9a56
AndyN9 <13814584+AndyN9@users.noreply.github.com>
2022-04-01 17:06:48 -0500

    refactor edges

--- src/SpendingFlowchart/spendingEdges.js
+++ src/SpendingFlowchart/spendingEdges.js
@@ -1,38 +1,37 @@
-const edgeStyle = {
-  type: 'smoothstep',
-  markerEnd: {
-    type: 'arrowclosed',
-  },
-};
-
-function parse(edgeId) {
-  const data = edgeId.split('-to-');
+function createFlow(edge) {
+  const defaultEdgeProperties = {
+    type: 'smoothstep',
+    markerEnd: {
+      type: 'arrowclosed',
+    },
+  };
+  const data = edge.id.split('-to-');
 
   return {
-    id: edgeId,
+    ...defaultEdgeProperties,
     source: data[0],
     target: data[1],
-    ...edgeStyle,
+    ...edge,
   };
 }
 
 const phase0Edges = [
-  'phase-0-step-0-to-phase-0-step-1',
-  'phase-0-step-1-to-phase-0-step-2',
-  'phase-0-step-2-to-phase-0-step-3',
-  'phase-0-step-3-to-phase-0-step-4',
-  'phase-0-step-4-to-phase-0-step-5',
-  'phase-0-step-5-to-phase-0-step-6',
-].map((edgeId) => parse(edgeId));
+  { id: 'phase-0-step-0-to-phase-0-step-1' },
+  { id: 'phase-0-step-1-to-phase-0-step-2' },
+  { id: 'phase-0-step-2-to-phase-0-step-3' },
+  { id: 'phase-0-step-3-to-phase-0-step-4' },
+  { id: 'phase-0-step-4-to-phase-0-step-5' },
+  { id: 'phase-0-step-5-to-phase-0-step-6' },
+].map((edge) => createFlow(edge));
 
 const phase1Edges = [
-  'phase-0-step-6-to-phase-1-step-0',
-  'phase-1-step-0-to-phase-1-step-1',
-].map((edgeId) => parse(edgeId));
+  { id: 'phase-0-step-6-to-phase-1-step-0' },
+  { id: 'phase-1-step-0-to-phase-1-step-1' },
+].map((edge) => createFlow(edge));
 
 const phase2Edges = [
-  'phase-1-step-1-to-phase-2-step-0',
-  'phase-2-step-0-to-phase-2-step-1',
-].map((edgeId) => parse(edgeId));
+  { id: 'phase-1-step-1-to-phase-2-step-0' },
+  { id: 'phase-2-step-0-to-phase-2-step-1' },
+].map((edge) => createFlow(edge));
 
 export default [...phase0Edges, ...phase1Edges, ...phase2Edges];
