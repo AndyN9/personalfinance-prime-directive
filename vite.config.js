367aba3ed8da2ab377b518f67b436171d0ae57b9
AndyN9 <13814584+AndyN9@users.noreply.github.com>
2022-04-08 08:29:16 -0500

    add working build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+export default defineConfig(() => ({
+  plugins: [
+    react({
+      jsxRuntime: 'classic',
+    }),
+    cssInjectedByJsPlugin({ topExecutionPriority: false }),
+  ],
+  build: {
+    outDir: 'docs',
+  },
+}));
